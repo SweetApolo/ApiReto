6b659b07ef36306450d5730267dd45c52976c617
Jojo <boovanegas@gmail.com>
2022-10-19 14:03:56 -0500

    Finalizacion

--- main.js
+++ main.js
@@ -1,34 +1,65 @@
-const pokeCard = document.querySelector('[data-card]');
-const pokeImgContainer = document.querySelector('[data-img-container]');
-const pokeImg = document.querySelector('[data-img]');
-const pokeName = document.querySelector('[data-name]');
-const pokeId = document.querySelector('[data-id]');
-const pokeTypes = document.querySelector('[data-types]');
+const nombrePokemon = document.querySelector('[pokemon-nombre]');
+const imagenPokemon = document.querySelector('[pokemon-imagen]');
+const numeroPokemon = document.querySelector('[pokemon-numero]');
+const tipoPokemon = document.querySelector('[pokemon-tipo]');
+const statsPokemon = document.querySelector('[pokemon-stats]');
 
-const buscaPoke = event => {
+
+
+const buscarPokemon = event => {
     event.preventDefault();
     const { value } = event.target.pokemon;
     fetch(`https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`)
         .then(data => data.json())
-        .then(response => dataPoke(response))
+        .then(response => dataPokemones(response))
+        .catch(err => renderNotFound())
 }
 
-const dataPoke = data => {
-    const sprite =  data.sprites.front_default;
-    const {types} = data;
-    console.log(data)
+const dataPokemones = data => {
+    const sprite = data.sprites.front_default;
+    const { types, stats } = data;
 
-    pokeName.textContent = data.name;
-    pokeImg.setAttribute('src', sprite);
-    pokeId.textContent = `Nº ${data.id}`;
-    typesPoke(types);
+    nombrePokemon.textContent = data.name;
+    imagenPokemon.setAttribute('src', sprite);
+    numeroPokemon.textContent = `Nº ${data.id}`;
+    
+    tipoPokemones(types);
+    statsPokemones(stats);
 }
 
-const typesPoke = types => {
-    pokeTypes.innerHTML = '';
+
+
+const tipoPokemones = types => {
+    tipoPokemon.innerHTML = '';
     types.forEach(type => {
         const typeTextElement = document.createElement("div");
         typeTextElement.textContent = type.type.name;
-        pokeTypes.appendChild(typeTextElement);
+        tipoPokemon.appendChild(typeTextElement);
     });
 }
+
+const statsPokemones = stats => {
+    statsPokemon.innerHTML = '';
+    stats.forEach(stat => {
+        const statElement = document.createElement("div");
+        const statElementName = document.createElement("div");
+        const statElementAmount = document.createElement("div");
+        statElementName.textContent = stat.stat.name;
+        statElementAmount.textContent = stat.base_stat;
+        statElement.appendChild(statElementName);
+        statElement.appendChild(statElementAmount);
+        statsPokemon.appendChild(statElement);
+    });
+}
+
+const renderNotFound = () => {
+    nombrePokemon.textContent = 'No encontrado';
+    imagenPokemon.setAttribute('src', 'pokeball-transparent-png-2.png');
+    imagenPokemon.style.background = '#fff';
+    tipoPokemon.innerHTML = '';
+    statsPokemon.innerHTML = '';
+    numeroPokemon.textContent = '';
+}
+
+
+
